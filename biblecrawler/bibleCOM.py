f70b8c1a8aa375d917ff5d7456f413f5c552db19
Ehsan Asgari <asgari@berkeley.edu>
2019-05-27 03:18:37 +0200

    fix biblecom problem

--- biblecrawler/bibleCOM.py
+++ biblecrawler/bibleCOM.py
@@ -167,7 +167,8 @@ class BibleCom(BibleCrawler,BibleParser):
         if len(result)>0:
             f = codecs.open(outputfile, 'w', 'utf-8')
             for i, v in result:
-                f.write('%s\t%s\n' % (re.findall(r'\d\d\d\d\d\d\d\d',i.replace('_','')), ' '.join(w for w in v.split(' ') if w)))
+                if len(re.findall(r'\d\d\d\d\d\d\d\d',i.replace('_','')))>0:
+                    f.write('salam%s\t%s\n' % (re.findall(r'\d\d\d\d\d\d\d\d',i.replace('_',''))[0], ' '.join(w for w in v.split(' ') if w)))
             f.close()
 
 
